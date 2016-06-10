d1a37aa57301b7b0e0102808b0f63c09b6d7348b
Elariand <thomas1.martin@epitech.eu>
2016-06-10 09:21:47 +0200

    get request working

--- app/user/userController.js
+++ app/user/userController.js
@@ -38,79 +38,37 @@ LeadisControllers.controller('userController', ['$scope', '$http', function($sco
 	{
 		if (checkDataForm() == false)
 			alert(alertMessage);
-		$http.post('http://163.5.84.111:2222/v0.1/api/account', newUser).success(function(data) {
-			alert("hum");
-			if (data.success)
+
+		$http({
+			method: 'POST',
+			url: 'http://163.5.84.111:2222/v0.1/api/account',
+			data: {
+						"Pseudo" : newUser.Pseudo,
+						"Password" : newUser.Password,
+						"Email" : newUser.Email,
+						"Name" : newUser.Name,
+						"FirstName" : newUser.FirstName
+					}
+			}).then(function successCallback(response) {
 				alert("success");
-			else
-				alert("fail");
-		}).error(function(a, err) {
-			alert("Error: " + err);
-		});
+	       		$scope.details = response.data;
+			}, function errorCallback(response) {
+				alert("Error: " + response.statusText + response.Message);
+			});
 	};
-//	Get account by Id
-//	{
-//	  "Email": "user@example.com",
-//	  "Name": "Smith",
-//	  "FirstName": "John",
-//	  "Pseudo": "Utilisateur"
-//	}
-	$scope.login_user = function()
+
+	$scope.login_user = function(nb)
 	{
 		$http({
 			method: 'GET',
-			url: 'http://163.5.84.111:2222/v0.1/api/account/1'
+			url: 'http://163.5.84.111:2222/v0.1/api/account/'+nb
 		}).then(function successCallback(response) {
-			alert("hum");
         	$scope.details = response.data;
-		  }, function errorCallback(response) {
+        }, function errorCallback(response) {
 			alert("Error: " + response.statusText);
-		  });
-
-		// $http.get('http://163.5.84.111:2222/v0.1/api/account/1').success(function(response) {
-		// 	alert("hum");
-  //       	$scope.details = response.data;
-  //       }).error(function(a, err) {
-		// 	alert("Error: " + err);
-		// });
-
-		// if (!(loginInfo.Pseudo && loginInfo.Password))
-		// 	alert(alertMessage);
-		// $http.get('http://163.5.84.111:2222/v0.1/api/account/:id', loginInfo).then(function(response) {
-		// 	alert("hum");
-  //       	$scope.details = response.data;
-  //       });
+		});
 	};
 
 	$scope.edit_user_info = function()
 	{	}
 }]);
-
-/*GuessController.controller('basicController', ['$scope', '$http', function($scope, $http) {
-    var message = $scope.message = "Welcome !";
-    var inputs = $scope.inputs = "";
-    var instanceId = 0;
-
-    $scope.CheckAnswer = function(input){
-        $http({
-            method: 'GET',
-            url: 'http://localhost:8181/api/game/player/answers/' + input,
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            })
-        .then(function successCallback(response) {
-            if (response.status == 200) {
-                if (instanceId != response.data.instanceID)
-                {
-                    alert("The game has been updated !");
-                    instanceId = response.data.instanceID;
-                }
-                if (response.data.result == true)
-                    $scope.message = "GG !";
-                else
-                    $scope.message = "You lost this time, try again dude :3";
-            }
-        }, function errorCallback(response) {
-            alert("errorCallback");
-        });
-    }
-}]);*/
